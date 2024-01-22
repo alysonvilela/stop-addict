5c35aa927ef07d7bed4bef5ee06521afcb5243b5
Alyson Vilela <ctt.alysonvilela@gmail.com>
2024-01-22 19:12:36 -0300

    feat: add infos before form

--- src/pages/Home/CreateUserModal/index.tsx
+++ src/pages/Home/CreateUserModal/index.tsx
@@ -8,6 +8,7 @@ import { InputNumber } from "../../../components/InputNumber";
 import { useAddictStore } from "../../../store/addict-store";
 import { useUserStore } from "../../../store/user-store";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const createUserSchema = z.object({
   name: z.string().min(2, "Adicione um valor válido"),
@@ -21,6 +22,8 @@ const createUserSchema = z.object({
 type FormValues = z.infer<typeof createUserSchema>;
 
 export const CreateUserModal = () => {
+  const [phase, setPhase] = useState(1);
+
   const {
     control,
     handleSubmit,
@@ -46,28 +49,57 @@ export const CreateUserModal = () => {
   });
 
   return (
-    <form onSubmit={onSubmit}>
-      <S.ModalWrapper>
-        <h1>Seja bem vindo!</h1>
-        <p>Para continuar, precisamos de algumas informações básicas</p>
-        <section>
-          <InputText label="Nome" error={errors?.name} {...register("name")} />
-          <InputText
-            label="O que você gostaria de diminuir a frequencia"
-            error={errors?.addict_name}
-            {...register("addict_name")}
+    <S.ModalWrapper>
+      {phase === 1 && (
+        <>
+          <h1>Antes de qualquer coisa</h1>
+          <p>
+            O propósito desse aplicativo é te ajudar a diminuir a frequência de
+            hábitos ruins durante o ano.
+          </p>
+          <p>
+            Há habitos que são tão prejudiciais que a ideia seria acabar com ele
+            de fato.
+          </p>
+          <p>
+            Então caso o experimento tenha sucesso nesse ano, no próximo tente
+            não usar mais esse app, mas sim eliminar o que te prejudica de vez
+          </p>
+          <Button
+            type="button"
+            text="entendi"
+            className="w-full"
+            onClick={() => setPhase(2)}
           />
-          <InputNumber
-            control={control}
-            label={`Quantidade maxima nesse ano`}
-            suffix="/ano"
-            {...register("yearQuantity")}
-          />
-        </section>
-        <div className="buttons">
-          <Button type="submit" text="confirmar" className="w-full" />
-        </div>
-      </S.ModalWrapper>
-    </form>
+        </>
+      )}
+      {phase === 2 && (
+        <form onSubmit={onSubmit}>
+          <h1>Seja bem vindo!</h1>
+          <p>Para continuar, precisamos de algumas informações básicas</p>
+          <section>
+            <InputText
+              label="Nome"
+              error={errors?.name}
+              {...register("name")}
+            />
+            <InputText
+              label="O que você gostaria de diminuir a frequencia"
+              error={errors?.addict_name}
+              {...register("addict_name")}
+            />
+            <InputNumber
+              control={control}
+              label={`Quantidade maxima nesse ano`}
+              suffix="/ano"
+              {...register("yearQuantity")}
+            />
+          </section>
+          <div className="buttons">
+            <Button type="submit" text="confirmar" className="w-full" />
+          </div>
+        </form>
+      )}
+    </S.ModalWrapper>
   );
 };
